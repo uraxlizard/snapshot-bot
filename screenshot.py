4360f31e07f4c44aea7c2ffaf663d1771241e48d
Angelo Boyarov <uraxlizard@gmail.com>
2025-01-28 09:40:43 +0200

    adding headless mode

--- screenshot.py
+++ screenshot.py
@@ -27,6 +27,7 @@ class YouTubeSearchScreenshot:
         """Set up the Chrome WebDriver using undetected_chromedriver."""
         try:
             options = uc.ChromeOptions()
+            options.add_argument("--headless")
             options.add_argument("--disable-gpu")
             options.add_argument("--window-size=1920,1080")
             self.driver = uc.Chrome(options=options)
